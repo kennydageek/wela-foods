9f54329a3c316c0fc8c53859c57735f3dc7265fd
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-11-22 10:13:34 +0100

    Vue starter files added and vuetify configured

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app id="app">
+    <router-view />
+  </v-app>
+</template>
+
+<style lang="scss" scoped>
+$bg-color: #f9f8f5;
+$font-primary: Neue Machina, serif;
+
+#app {
+  background: $bg-color;
+  font-family: $font-primary;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>HomeView</h1>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {},
+};
+</script>
