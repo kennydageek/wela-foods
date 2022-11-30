97ce0cac7a6359414818b7ccd6944622bb6d8356
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-11-30 01:31:26 +0100

    footer contact component implemented

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="footer-wrapper">
     <h1 class="footer__heading">A New Diet Model</h1>
-    <!-- <div v-for="(social, i) in socials" :key="i"> -->
-    <social-icon />
-    <!-- </div> -->
+    <social-icon class="social-icon" />
+
+    <v-row>
+      <v-col cols="5" class="first-col">
+        <p class="first-col__heading">Wela</p>
+        <p class="first-col__paragraph">
+          We are a global food organization offering food prduce and tutorials
+        </p>
+
+        <footer-contact />
+      </v-col>
+      <v-col cols="5" class="second-col">;lldldlddldl</v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 import socialIcon from './SocialIcons.vue';
+import FooterContact from './FooterContact.vue';
 export default {
   components: {
     socialIcon,
+    FooterContact,
   },
 };
 </script>
@@ -27,5 +39,30 @@ export default {
   font-weight: 800;
   font-size: 4rem;
   margin-bottom: 10rem;
+}
+
+.social-icon {
+  margin-bottom: 10rem;
+}
+
+.first-col {
+  /* background: blue; */
+}
+
+.first-col__heading {
+  font-family: 'neue machina';
+  color: var(--color-accent);
+  font-size: 2.8rem;
+  margin-bottom: 1.2rem;
+}
+
+.first-col__paragraph {
+  color: #9a9ea6;
+  font-size: 1.4rem;
+  margin-bottom: 2.4rem;
+}
+
+.second-col {
+  background: purple;
 }
 </style>
---
+++ src/components/FooterContact.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="footer-flex">
+    <div class="footer-contact-wrapper" v-for="(item, i) in items" :key="i">
+      <img :src="item.src" alt="" />
+      <div class="details">
+        <p class="details__heading">{{ item.heading }}</p>
+        <p class="details__contact">{{ item.contact }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: require('@/assets/images/icons/phone.svg'),
+          heading: 'Have a question?',
+          contact: '3104372766',
+        },
+        {
+          src: require('@/assets/images/icons/mail.svg'),
+          heading: 'Contact us at',
+          contact: 'wela@outlook.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.footer-flex {
+  display: flex;
+  /* justify-content: space-between; */
+}
+
+.footer-contact-wrapper {
+  display: flex;
+}
+
+.footer-contact-wrapper:not(:first-of-type) {
+  margin-left: 4rem;
+}
+
+img {
+  align-self: center;
+}
+
+.details {
+  align-self: center;
+  margin-left: 1rem;
+}
+
+.details__heading {
+  color: #9a9ea6;
+  margin-bottom: 0;
+  font-size: 1.2rem;
+}
+
+.details__contact {
+  color: var(--color-primary);
+  font-size: 1.6rem;
+  margin-bottom: 0;
+  font-weight: 600;
+}
+</style>
